9592273d38bbf58086e35fbc653d147cd5e0d9a0
Leonardo Faria <leonardofarias1806@gmail.com>
2019-09-02 22:14:26 -0300

    ajustes readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# Projeto de estudo
+
 # Calculadora JavaScript
 
 [![Hcode Treinamentos](https://www.hcode.com.br/res/img/hcode-200x100.png)](https://www.hcode.com.br)
--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -126,7 +126,13 @@ class CalcController {
     }
 
     getResult(){
-        return eval(this._operation.join(""));
+        try{
+            return eval(this._operation.join(""));
+        }catch(e){
+            setTimeout(() => {
+                this.setError();
+            }, 1)
+        }
     }
 
     calc() {
@@ -398,6 +404,12 @@ class CalcController {
     }
 
     set displayCalc(value){
+        
+        if (value.toString().length > 10) {
+            this.setError();
+            return false;
+        }
+
         this._displayCalcEl.innerHTML = value;
     }
 
